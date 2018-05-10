f9812c44109510fee5894eafc0d8dbf1c87b40f2
Hanzone <githug@163.com>
2018-05-10 19:59:52 +0800

    try pycharm

--- fun.py
+++ fun.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # Author:Hanzone Hao
 
-import fun
 
-print("hello, test world!")
+def main():
+    print("hello, fun world!")
 
 
 if __name__ == '__main__':
-    fun.main()
-
+    main()
